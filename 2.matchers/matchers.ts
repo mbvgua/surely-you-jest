285332735bea23fb495f07920bbeb2c0321a5959
mbvgua <mbuguadouglas17@gmail.com>
2024-11-28 14:08:17 +0300

    understood on using matcher to conduct simple assertions on basic values

---
+++ 2.matchers/matchers.ts
@@ -0,0 +1,14 @@
+
+
+export function sum(a:number, b:number) {
+    return a + b
+}
+
+
+export function dummyFunc(){
+    // throw new Error('what you know is a drop. What you dont know is the ocean')
+    throw new Error('Oops! Wrong input')
+}
+
+
+
