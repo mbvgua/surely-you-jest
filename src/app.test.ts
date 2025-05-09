a32d4b33d3bcdbe33a99e4bcb7a5bd641d362133
mbvgua <105845499+mbvgua@users.noreply.github.com>
2025-05-09 10:57:16 +0300

    Add a test for the database connection

--- src/app.test.ts
+++ src/app.test.ts
@@ -1,21 +1,28 @@
 import app from "./app";
 import supertest from "supertest";
+import { pool } from "./db";
 
-const request = supertest
+const api = supertest(app)
 
 describe("[app setup]", ()=>{
 
     test("the server is running", async()=>{
-        expect(app).toBeDefined()
+        expect(api).toBeDefined()
     })
 
-    // database connection is successful
+    test('the database connection is successful', async()=>{
+        const [rows]:any = await pool.query(
+            `SELECT 1+1 as result;`
+        )
+        expect(rows[0]).toMatchObject({
+            result:2
+        })
+    })
 
     test("unkown routes get 404 error handler", async()=>{
         const randomn_string = 'qwewrqdasas'
-        const response = await request(app).get(`/${randomn_string}`)
+        const response = await api.get(`/${randomn_string}`)
         expect(response.statusCode).toBe(404)
     })
 
-    // parse-json in request body
 })
